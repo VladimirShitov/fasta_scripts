9681f000583bedfca0a237439b0543e4a57717fd
Vladimir Shitov <35199218+VladimirShitov@users.noreply.github.com>
2019-09-03 16:10:21 +0800

    Added get_gene_info()

--- fasta_scripts.py
+++ fasta_scripts.py
@@ -53,3 +53,65 @@ def parse_phobius(op, print_names = True):
 			sep = row.find(' Y ')
 		split_phobius_op(row[sep+3:])
 
+def get_gene_info(gene_id, organism, email):
+	"""
+	Get gene short name and information by given id.
+	Perform NCBI gene search twice: for a given id (it is a locus tag, probably) and for UID found. If given
+	gene_id is not found in 'gene' database, performs search in 'protein' database. Requires Biopython installed.
+	Parameters
+	----------
+	gene_id : str
+ 	Name to search in NCBI gene database. I.e. 'NP_414542.1'
+	organism: str
+    	A particular species to search in database. I.e. 'Escherichia coli'
+	email : str
+    	Your email adress to tell NCBI who you are    
+	Returns
+	-------
+	name : str
+    	Attribute 'Name' from summary for found gene. Usually a short name like 'ytjA'. None if not found.
+	info : str
+    	Attribute 'Summary' from summary for found gene. Brief description of a gene. 'Not found' if not found.
+	Examples
+	--------
+	>>> name, info = get_gene_info('NP_414542.1', 'Escherichia coli', 'vladimirs@intern.bii.a-star.edu.sg')
+	>>> print(name)
+	thrL
+	>>> print(info)
+	The ThrL leader peptide controls by attenuation the expression of the  thrLABC operon,
+	which encodes four out of the five enzymes of threonine biosynthesis pathway,
+	in response to the threonine and isoleucine levels . [More information is available at EcoCyc: EG11277].
+	"""
+	from Bio import Entrez
+	Entrez.email = EMAIL
+    
+	db = 'gene'
+
+	search = Entrez.esearch(db, '{}[orgn] {}'.format(organism, gene_id))
+	record = Entrez.read(search)
+
+	if not record['IdList']:
+    		db = 'protein'
+
+    	search = Entrez.esearch(db, '{}[orgn] {}'.format(organism, gene_id))
+    	record = Entrez.read(search)
+
+    	if not record['IdList']:
+        	return None, 'Not found'
+
+	gene_id = record['IdList'][0]
+
+	handle = Entrez.esummary(db=db, id=gene_id)
+	record = Entrez.read(handle)
+
+	if db == 'gene':
+		summary_dict = record['DocumentSummarySet']['DocumentSummary'][0]
+		name = summary_dict['Name']
+		info = summary_dict['Summary']
+
+	elif db == 'protein':
+		summary_dict = record[0]
+		name = summary_dict['Title']
+		info = summary_dict['Comment']
+
+	return name, info
